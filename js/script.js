b100e2e02392889b67399e6862624d9a6cc9e718
FedericoV <vaglicafederico95@gmail.com>
2020-07-08 16:13:39 +0200

    2 commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 //Creare un array di 3 elemnti chiedere un numero all utento e visualizzare l - elemnto desiderato
 
 
+
+
+
 var email = ['peppe@gmail.com' , 'mario@gmail.com', 'luigi@gmail.com'];
 var userEmail = prompt('inserisci la tu email');
+var emailTrovata = false
 
 for (i=0; i < email.length ; i++) {
-    if (userEmail == email[i]) {
-        alert('Exist');
-        break;  
-    } else {
-        alert('Not exist');
-        break;        
-    }
+    if(userEmail == email[i]){
+        emailTrovata = true 
+    } else {}
 }
+
+if (emailTrovata == true) {
+    alert('Benvenuto')
+} else {
+    alert('Perfavore registrati')
+}
+
